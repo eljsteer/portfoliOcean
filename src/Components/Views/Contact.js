c595a781d3329c4a30a97e137c7b8bda1598ec96
Jason <el.jsteer@gmail.com>
2022-10-08 20:07:02 +1100

    updated to complete contact complete valisation and UX if user's input is not valid

--- src/Components/Views/Contact.js
+++ src/Components/Views/Contact.js
@@ -8,25 +8,39 @@ function Contact() {
     email: "",
     message: "",
   });
+// for validation states: 2 = Unanswered, 0 = InValid, 1 = Valid, 
+  const [valState, setValState] = useState({Name:2, Email:2, Message:2}) 
   const [errorMessage, setErrorMessage] = useState("");
-  const { Name, Email, Message } = formState;
+  // const { Name, Email, Message } = formState;
 
-  function handleFormChange(event) {
-    if (event.target.name === "email") {
+  function handleFormChange(event) {  
+    if (event.target.name === "Email") {
         const isValid = validateEmail(event.target.value);
         if (!isValid) {
             setErrorMessage("A Valid Email is Required");
-
-        } else {
+            setValState({Email:0, Name: valState.Name, Message: valState.Message})
+          } else {
             setErrorMessage("");
+            setValState({Email:1, Name: valState.Name, Message: valState.Message})
+
+
         }
-    } else {
+    } else if (event.target.name === "Name") {
         if (!event.target.value.length) {
             setErrorMessage(`${event.target.name} is a required field`);
+            setValState({Email: valState.Email, [event.target.name]: 0, Message: valState.Message})
         } else {
             setErrorMessage("");
+            setValState({Email: valState.Email, [event.target.name]: 1, Message: valState.Message})
         }
-    }
+    } else 
+    if (!event.target.value.length) {
+      setErrorMessage(`${event.target.name} is a required field`);
+      setValState({Email: valState.Email, [event.target.name]: 0, Name: valState.Name})
+  } else {
+      setErrorMessage("");
+      setValState({Email: valState.Email, [event.target.name]: 1, Name: valState.Name})
+  }
     if (!errorMessage) {
         setFormState({ ...formState, [event.target.name]: event.target.value });
     }
@@ -36,7 +50,6 @@ function Contact() {
   function handleFormSubmit(event) {
     event.preventDefault();
     if (!errorMessage) {
-
         console.log(formState);
     }
   }
@@ -47,23 +60,23 @@ function Contact() {
         <h2 className="moreSpace ContactHeader">Contact Me</h2>
       </div>
       <div id="contactContainer row">
-        <form id="formContainer" className="form container col-md-6 needs-validation" noValidate>
+        <form id="formContainer" className="form container col-md-6 needs-validation" novalidate>
           <div className="container d-flex justify-content-center">
             <div className="form-group col-lg-6">
               <label htmlFor="validationDefault01" className="contactLabels form-label">Name:</label>
-              <input type="text" className="form-control" id="validationDefault01" name="Name" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter name" required/>
+              <input type="text" className={ valState.Name===2?'form-control': (valState.Name===1?"form-control is-valid":"form-control is-invalid")} id="validationDefault01" name="Name" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter name" required/>
             </div>
           </div> 
           <div className="container d-flex justify-content-center">
             <div className="form-group col-lg-6">
               <label htmlFor="validationDefaultEmail" className="contactLabels">Email address:</label>
-              <input type="email" className="form-control" id="validationDefaultEmail" name="Email" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter email" required/>
+              <input type="email" className={ valState.Email===2?'form-control': (valState.Email===1?"form-control is-valid":"form-control is-invalid")} id="validationDefaultEmail" name="Email" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter email" required/>
             </div>
           </div>
           <div className="container d-flex justify-content-center">
             <div className="form-group col-lg-8">
               <label htmlFor="validationDefaultMessage" className="contactLabels">Message:</label>
-              <textarea type="text" className="form-control is-valid" id="validationDefaultMessage" name="Message" onBlur={handleFormChange} rows="4" aria-live="assertive" placeholder="Please enter your message " required/>
+              <textarea type="text" className={ valState.Message===2?'form-control': (valState.Message===1?"form-control is-valid":"form-control is-invalid")} id="validationDefaultMessage" name="Message" onBlur={handleFormChange} rows="4" aria-live="assertive" placeholder="Please enter your message " required/>
             </div>
           </div>
           <br />
