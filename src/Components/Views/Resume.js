e93e294063c0fb3861b0a5792b67506d251fecc0
Jason <el.jsteer@gmail.com>
2022-10-09 18:52:46 +1100

    final bit of foratting and styling for resume page

--- src/Components/Views/Resume.js
+++ src/Components/Views/Resume.js
@@ -6,26 +6,44 @@ function Resume() {
   // const toggleHover = () => setHovered(!hovered);
   // const classChange = hovered ? "hide": "show" ;
   return (
-    <div>
-        <h1 className="moreSpace pageHeader">Resume</h1>
-        <div className="d-flex justify-content-center">
-          <div id="skillsContainer" className="d-flex flex-row col-xxl-4 col-xl-6 col-lg-6 col-md-8 col-sm-10 wrapper justify-content-center" >
-            {SkillsData.map(skills => (
-              <div 
-                // onMouseEnter={toggleHover}
-                // onMouseLeave={toggleHover}
-                >
+    <div class="d-flex container text-center justify-content-center">
+      <div class="d-flex flex-column">
+        <div>
+          <h1 className="moreSpace pageHeader">Resume</h1>
+        </div>
+        <div className="d-flex flex-row justify-content-center">
+          <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-8">
+            {/* <h3 className="pageHeader">Web Development Proficiencies</h3> */}
+            <div id="skillsContainer" className="d-flex flex-row wrapper justify-content-center" >
+              {SkillsData.map(skills => (
+              <div // onMouseEnter={toggleHover} // onMouseLeave={toggleHover}
+                  >
                 <img className="resumeIcon" src={process.env.PUBLIC_URL + `${skills.icon}`} alt={skills.name}/>
                 <div id="skillNames" className="hide">{skills.name}</div>
               </div>
-              ))}
+                ))}
+              <div className="resumeDownload">
+                <h3 id="subtitle">Download Resume</h3>
+                <div>
+                  <a href={resumeFile.file} download>
+                    <button istype="button" className="btn btn-outline-light" id="downloadBttn">Download</button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
+
+          {/* <div class="col-6">
+            <h3 className="pageHeader">Download Resume</h3>
+            <div>
+              <a href={resumeFile.file} download>
+                <button istype="button" className="btn btn-outline-light" id="downloadBttn">Download</button>
+              </a>
+            </div>
+          </div> */}
         </div>
-        <h1 className="moreSpace">My Web Development Skills</h1>
-        <a href={resumeFile.file} download>
-          <button istype="button" className="btn btn-outline-light" id="downloadBttn">Download</button>
-        </a>
+      </div>
     </div>
-    );
+  )
 }
 export default Resume;
