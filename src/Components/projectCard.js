29914ba75a8e25802713dca6f6dedd29db14d326
Jason <el.jsteer@gmail.com>
2022-10-11 18:10:55 +1100

    updated image paths

--- src/Components/projectCard.js
+++ src/Components/projectCard.js
@@ -5,7 +5,7 @@ function ProjectCard({project}) {
     return (
       <div className="projectBox card col-lg-4 col-md-6 col-sm-8 col-xs-10 m-4 p-0" key={project.name}>
         {/* <img className="projectImg" src={`${process.env.PUBLIC_URL}/assets/${project.id}.jpg`} alt={project.alt}/> */}
-        <img className="projectImg" src={process.env.PUBLIC_URL + project.image} alt={project.alt}/>
+        <img className="projectImg" src={process.env.PUBLIC_URL + "/" + project.image} alt={project.alt}/>
         <div className="imageOverlay card-img-overlay">
           <h5 className="card-title">{project.name}</h5>
           <p>{project.description}</p>
