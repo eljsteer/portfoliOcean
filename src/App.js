474bd43a1a2c5a7141694d90c81e476268406d5c
Jason <el.jsteer@gmail.com>
2022-12-18 17:58:10 +1100

    update to routes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Router>
           <Header />   
             <Routes>
-              <Route path="/" index element={ < Home/> } /> 
+              <Route path="/" element={ < Home/> } /> 
               <Route path="/portfolio" element={<Projects />} />         
               <Route path="/about" element={<About />}/>              
               <Route path="/contact" element={<Contact />} />
