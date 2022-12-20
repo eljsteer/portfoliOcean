4f94a5c2055ae079bfef03edc73403b55ddd6a8c
Jason <el.jsteer@gmail.com>
2022-12-20 18:06:57 +1100

    updated contact me page to send me an email message from the form. Still need to add a confirmation modal etc once message sends

--- src/Components/Views/Contact.js
+++ src/Components/Views/Contact.js
@@ -7,9 +7,11 @@ function Contact() {
   function handleFormSubmit(event) {
     event.preventDefault();
     if (!errorMessage) {
-      console.log(formState);
-      emailjs.sendForm('service_iyn7iwb', 'template_ub5v26a', form.current, 'GumlAi18HzddL4L07')
+      emailjs.sendForm('service_iyn7iwb', 'template_e78547h', form.current, 'GumlAi18HzddL4L07')
       .then((result) => {
+        if(result.text === "OK") {
+          messageSent();
+        };
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
@@ -17,6 +19,10 @@ function Contact() {
     } else if (errorMessage) {
       console.log(errorMessage);
     }
+  }
+
+  function messageSent() {
+
   }
 
   const [formState, setFormState ] = useState({
@@ -52,14 +58,14 @@ function Contact() {
     if (!event.target.value.length) {
       setErrorMessage(`${event.target.name} is a required field`);
       setValState({Email: valState.Email, [event.target.name]: 0, Name: valState.Name})
-  } else {
+    } else {
       setErrorMessage("");
       setValState({Email: valState.Email, [event.target.name]: 1, Name: valState.Name})
-  }
+    }
     if (!errorMessage) {
         setFormState({ ...formState, [event.target.name]: event.target.value });
     }
-    }
+  }
 
   return (
     <div className="container-fluid flex-column d-flex">
@@ -67,32 +73,17 @@ function Contact() {
         <h2 className="moreSpace pageHeader">Contact Me</h2>
       </div>
       <div id="contactContainer">
-        {/* <div className="detailsContainer col-md-4">
-
-        </div> */}
-        <form ref={form} id="formContainer" className="form container col-xxl-4 col-xl-6 col-lg-6 col-md-8 needs-validation" novalidate>
-          <div className="container d-flex justify-content-center">
-            <div className="form-group col-lg-6">
-              <label htmlFor="validationDefault01" className="contactLabels form-label">Name:</label>
-              <input type="text" className={ valState.Name===2?'form-control': (valState.Name===1?"form-control is-valid":"form-control is-invalid")} id="validationDefault01" name="Name" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter name" required/>
-            </div>
-          </div> 
-          <div className="container d-flex justify-content-center">
-            <div className="form-group col-lg-6">
-              <label htmlFor="validationDefaultEmail" className="contactLabels">Email address:</label>
-              <input type="email" className={ valState.Email===2?'form-control': (valState.Email===1?"form-control is-valid":"form-control is-invalid")} id="validationDefaultEmail" name="Email" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter email" required/>
-            </div>
-          </div>
-          <div className="container d-flex justify-content-center">
-            <div className="form-group col-lg-8">
-              <label htmlFor="validationDefaultMessage" className="contactLabels">Message:</label>
-              <textarea type="text" className={ valState.Message===2?'form-control': (valState.Message===1?"form-control is-valid":"form-control is-invalid")} id="validationDefaultMessage" name="Message" onBlur={handleFormChange} rows="4" aria-live="assertive" placeholder="Please enter your message " required/>
-            </div>
-          </div>
-          <br />
-          <button type="submit" className="btn btn-outline-light col-lg-3 col-md-3 col-sm-2" onSubmit={handleFormSubmit}  value="Send" id="submitButton">Submit</button>
-          {errorMessage && (<p>{errorMessage}</p>)}
+        <form ref={form} onSubmit={handleFormSubmit} id="formContainer" className="form container col-xxl-4 col-xl-6 col-lg-6 col-md-8 needs-validation">
+          <label htmlFor="validationDefault01" className="contactLabels form-label">Name:</label>
+          <input type="text" className={ valState.Name===2?'form-control': (valState.Name===1?"form-control is-valid":"form-control is-invalid")} id="validationDefault01" name="Name" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter name" required/>
+          <label htmlFor="validationDefaultEmail" className="contactLabels">Email address:</label>
+          <input type="email" className={ valState.Email===2?'form-control': (valState.Email===1?"form-control is-valid":"form-control is-invalid")} id="validationDefaultEmail" name="Email" onBlur={handleFormChange} aria-live="assertive" placeholder="Enter email" required/>
+          <label htmlFor="validationDefaultMessage" className="contactLabels">Message:</label>
+          <textarea type="text" className={ valState.Message===2?'form-control': (valState.Message===1?"form-control is-valid":"form-control is-invalid")} id="validationDefaultMessage" name="Message" onBlur={handleFormChange} rows="4" aria-live="assertive" placeholder="Please enter your message " required/>
+          <br/>
+          <input type="submit" value="Send" className="btn btn-outline-light col-lg-3 col-md-3 col-sm-2 mb-4" />
         </form>
+        {errorMessage && (<p>{errorMessage}</p>)}
       </div>
     </div>
   );
